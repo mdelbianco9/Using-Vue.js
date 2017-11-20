fe569d9256db2839ecda563efc1ec95d64b9ad74
marcus delbianco <mdelbianco9@gmail.com>
2017-11-19 16:39:35 -0800

    Set up app.vue file with header and footer. Then linked to home page using router

---
+++ src/App.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="app">
+
+
+    <div class="navbar has-shadow">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">MockMeUp</a>
+
+          <!-- the v-on toggles the hamburger, the v-bind binds the hamburger with the router-links below -->
+          <button class="navbar-burger" v-on:click="toggleNav" v-bind:class="{ 'is-active':isActive }">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+
+        
+
+        <div class="navbar-end navbar-menu" v-bind:class="{ 'is-active':isActive }">
+
+          <router-link to="/" class="navbar-item r-item">Home</router-link>
+          <router-link to="Faq" class="navbar-item r-item">Features</router-link>
+          <router-link to="Faq" class="navbar-item r-item">About</router-link>
+          <router-link to="Faq" class="navbar-item r-item">FAQ</router-link>
+
+          <div class="navbar-item">
+            <p class="control">
+                <a class="button is-primary is-outlined">
+                  <span class="icon">
+                    <i class="fa fa-download"></i>
+                  </span>  
+                  <span>Join Now</span>
+                </a>
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+
+    
+    <!-- This is where the home and FAQ will reside -->
+    <router-view></router-view>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>This is a- spiffy footer for you Mr. Bond</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </div>
+</template>
+
+
+
+
+<script>
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      isActive: false
+    }
+  },
+
+  // this toggles between true and false for the isActive function. 
+
+  methods: {
+    toggleNav: function() {
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mq'
+
+
+.navbar 
+  background-color: #383838
+  a:hover
+    color: grey
+
+.navbar-brand a
+  color: #fff
+  font-weight: bold
+
+
+a.r-item
+  color: #C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile 
+    color: grey
+    &:hover 
+      background-color: #F1F1F1
+
+.navbar-burger
+  background-color: #383838
+  border: none
+  color: #F1F1F1
+
+footer 
+  background-color: $primary !important
+  color: #fff
+
+  .icon 
+    color: #fff
+    margin-right: 20px
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="home">
+     <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4"> {{ subheading }}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn">Learn More</a>
+        </div>
+      </div>
+     </section>
+
+     <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+
+          <div class="column is-1 has-text-centered">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+
+          <div class="column is-one-third-desktop">
+            <p class="title"><strong>We provide superior logistics so your business can succeed in crazy world.</strong></p>
+          </div>
+
+          <div class="column">
+            <p>Lorem Ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I thinks its an exelent tool for business. I cant survive without it."</p>
+              <p class="subtitle">Gary Bungus</p>
+            </div>
+          </div>
+        </div>
+      
+
+      
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I thinks its an exelent tool for business. I cant survive without it."</p>
+              <p class="subtitle">Gary Bungus</p>
+            </div>
+          </div>
+        </div>
+      
+
+      
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I thinks its an exelent tool for business. I cant survive without it."</p>
+              <p class="subtitle">Gary Bungus</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+     </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      heading: "Getting Practice with MockUps",
+      subheading: "Lorem Ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+
+.hero 
+  background: url('../assets/hero.jpg')
+  background-size: cover
+
+  .title 
+    color: #fff
+    +mobile
+      font-weight: bold
+    +tablet
+      font-weight: 2.5rem
+    +desktop
+      font-weight: 7rem
+      margin-top: 2rem
+
+h2 
+  margin: 1.5rem 0 2rem 0 !important
+  color: #fff
+
+.fa-cog 
+  font-size: 40px
+
+#learn
+  +desktop
+    margin-bottom: 2rem
+
+.pd
+  +tablet
+    padding: 2em 0
+  +desktop
+    padding: 2em 0
+
+.title
+  font-weight: 200;
+
+
+</style>
